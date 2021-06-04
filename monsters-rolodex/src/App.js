6fe7d8e986344e9dae90a30e5eae1a42a33a5bbe
ThriledLokki983 <gnimoh001@gmail.com>
2021-06-05 01:06:21 +0200

    implement searchFields

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 		super();
 		this.state = {
 			monsters: [],
+			searchField: "",
 		};
 	}
 
@@ -19,9 +20,19 @@ class App extends Component {
 	}
 
 	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((el) =>
+			el.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+
 		return (
 			<div className="App">
-				<CardList monsters={this.state.monsters} />
+				<input
+					type="search"
+					placeholder="search monsters"
+					onChange={(e) => this.setState({ searchField: e.target.value })}
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
