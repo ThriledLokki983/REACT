43f636d14c1afa008bee01277124acb889dce421
ThriledLokki983 <gnimoh001@gmail.com>
2021-06-04 23:06:42 +0200

    create-react-app

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,22 +1,25 @@
+/** @format */
+
 import React, { Component, component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			string: "Hello Gideon! is here",
+		};
+	}
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<p>My name is Gideon. change now!</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
+					<p>{this.state.string}</p>
+					<button onClick={() => this.setState({ string: "Akwaaba, Kwame" })}>
+						Change Text
+					</button>
 				</header>
 			</div>
 		);
